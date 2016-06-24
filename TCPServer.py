10d7258f16e48e45e8fb5e64fcde9c95ac13885b
AyberkHalac <AyberkHalac@users.noreply.github.com>
2016-06-24 17:42:33 +0300

    Last

--- TCPServer.py
+++ TCPServer.py
@@ -10,20 +10,22 @@ def isInteger(value):
         return False
 
 
-with open('nameListSorted.txt', encoding='utf-8') as f:
+with open('output.txt', encoding='utf-8') as f:
     lines = f.readlines()
 f.close()
 nameList = []
+surnameList = []
 idList = []
 mailList = []
+
 for line in lines:
     indexes = [i for i, x in enumerate(line) if x == ' ']
-    idList.append(line[:indexes[0]])
-    nameList.append(line[indexes[0] + 1:line.index('Bilgisayar')])
-    mailList.append(line[indexes[len(indexes) - 1] + 1:])
+    idList.append(line[indexes[len(indexes) - 2] + 1:indexes[len(indexes) - 1]])
+    surnameList.append(line[indexes[len(indexes) - 3] + 1:indexes[len(indexes) - 2]])
+    nameList.append(line[:indexes[len(indexes) - 3] + 1])
+    mailList.append(str(line[indexes[len(indexes) - 1] + 1:]).strip())
 
 serverPort = input('sunucu <ServerPort> : ')
-# serverPort = 'sunucu 9988'
 if serverPort.count(' ') == 1 and serverPort[:serverPort.index(' ')] == 'sunucu' and isInteger(
         serverPort[serverPort.index(' ') + 1:]):
 
@@ -48,18 +50,19 @@ if serverPort.count(' ') == 1 and serverPort[:serverPort.index(' ')] == 'sunucu'
                 recvSlot = int(recvSlot) - 1
                 if recvSlot < 44 and recvSlot >= 0:
                     c.send(
-                        ('StudenID: ' + idList[recvSlot] + '  StudentName: ' + nameList[recvSlot] + '  StudentMail: ' +
+                        ('StudenID: ' + idList[recvSlot] + '  StudentName: ' + nameList[
+                            recvSlot] + '  StudentSurname: ' + surnameList[recvSlot] + '  StudentMail: ' +
                          mailList[recvSlot] +
-                         'Herhangi bir degisiklik icin lutfen "ID=<>,Name=<>,Mail=<>" formatinda giris yapiniz.').encode(
+                         '\nHerhangi bir degisiklik icin lutfen "ID=<>,Name=<>,Surname=<>,Mail=<>" formatinda giris yapiniz.').encode(
                             'utf-8'))
                     try:
                         c.settimeout(30)
                         recv = (c.recv(1024)).decode("utf-8")
                         if recv != None:
-                            print(recv)
                             recvParser = [i for i, x in enumerate(recv) if x == ',']
                             id = recv[recv.index('ID=<') + 4:recvParser[0] - 1]
                             name = recv[recv.index('Name=<') + 6:recvParser[1] - 1]
+                            surname = recv[recv.index('Surname=<') + 9:recvParser[2] - 1]
                             mail = recv[recv.index('Mail=<') + 6:-1]
                             if id != '':
                                 idList[recvSlot] = id
@@ -67,26 +70,28 @@ if serverPort.count(' ') == 1 and serverPort[:serverPort.index(' ')] == 'sunucu'
                                 nameList[recvSlot] = name
                             if mail != '':
                                 mailList[recvSlot] = mail
+                            if surname != '':
+                                surnameList[recvSlot] = surname
                             with open("output.txt", "wt", encoding='utf-8') as out_file:
                                 for index in range(44):
                                     out_file.writelines(
-                                        nameList[index] + '  ' + idList[index] + '  ' + mailList[index] + '\r\n')
+                                        nameList[index] + '' + surnameList[index] + ' ' + idList[index] + ' ' +
+                                        mailList[index] + '\n')
                             print('File Printed.')
+                            c.send(('-1').encode('utf-8'))
                     except:
                         print('Connection Timeout or Error in Input-> Socket Closed-> Ip:', addr[0], '  Port:',
                               addr[1], '  Time:',
                               strftime("%Y-%m-%d %H:%M:%S"))
-                        continue
+                        # continue
 
                 else:
-                    c.send(('Input has to lower than 45 bigger than 0!').encode('utf-8'))
-                    c.close()
-                    continue
+                    break
             else:
                 print('Wrong Input -> ', addr[0])
-                c.send(('Wrong Input!').encode('utf-8'))
+                serverSocket.send(('-1').encode('utf-8'))
                 c.close()
-                continue
+                # continue
         except:
             continue
 else:
