96d09cb2cf49629c10810cf05af295c83627cf15
AyberkHalac <AyberkHalac@users.noreply.github.com>
2016-06-24 17:43:09 +0300

    Update UDPclient.py

--- UDPclient.py
+++ UDPclient.py
@@ -1,3 +1,4 @@
+import re
 import socket
 
 
@@ -8,26 +9,47 @@ def isInteger(value):
     except:
         return False
 
+
 Input = input('istemci <ServerIp> <ServerPort>: ')
-#Input = 'istemci 10.2.37.23 9988'
 indexes = [i for i, x in enumerate(Input) if x == ' ']
+
+p = re.compile(
+    '''^ID=<(1[0-9]{8}){0,1}>,Name=<([a-zA-ZçÇğĞıİöÖüÜşŞ]+){0,1}>,Surname=<([a-zA-ZçÇğĞıİöÖüÜşŞ]+){0,1}>,Mail=<(st1[0-9]{8}@etu.edu.tr){0,1}>$''')
 
 if len(indexes) == 2 and Input[:indexes[0]] == 'istemci' and isInteger(Input[indexes[1] + 1:]):
     serverIp = Input[indexes[0] + 1: indexes[1]]
-    serverPort = Input[indexes[1] + 1:]
-    try:
-        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serverSocket.connect((serverIp, int(serverPort)))
-    except:
-        print("Ip or Port is Wrong. Cannot Connect to Server")
+    serverPort = int(Input[indexes[1] + 1:])
+    index = ''
     print('Connection is started')
+    while True:
+        index = input('Lutfen index numarasi giriniz: ')
+        if not isInteger(index) or int(index) < 1 or int(index) > 45:
+            print('Input has to lower than 45 bigger than 0!')
+        else:
+            break
 
-    index = input('Lutfen index numarasi giriniz: ')
-    serverSocket.send((index).encode('utf-8'))
-    recv = (serverSocket.recv(1024)).decode("utf-8")
-    print(recv)
-    change = input('>>')
-    serverSocket.send((change).encode('utf-8'))
-    serverSocket.close
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto((index).encode('utf-8'), (serverIp, serverPort))
+        recvSlot, addr = sock.recvfrom(1024)
+        recvSlot = recvSlot.decode('utf-8')
+        print(recvSlot)
+        change = ''
+        while True:
+            change = input('>>')
+            if p.match(change):
+                break
+            else:
+                print('wrong INPUT')
+        sock.settimeout(30)
+        while True:
+            sock.sendto((change).encode('utf-8'), (serverIp, serverPort))
+            recvSlot, addr = sock.recvfrom(1024)
+            recvSlot = recvSlot.decode('utf-8')
+            if recvSlot == '-1':
+                print('İslem tamamlandi.')
+                sock.close()
+    except:
+        exit(0)
 else:
     print('Wrong input!')
